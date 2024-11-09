41392bfdcc051e65f3fa7ec7cb5f346c2b49816e
KadeSan4uk <kotejchik@gmail.com>
2024-11-09 21:32:17 +0200

    Revert

--- Assets/Scripts/SphereScaler.cs
+++ Assets/Scripts/SphereScaler.cs
@@ -1,26 +1,51 @@
+using System;
 using UnityEngine;
 
 public class SphereScaler : MonoBehaviour
 {
     private Vector3 _originalScale;
+
+    [SerializeField] private float _maxScale = 3.0f;
+    [SerializeField] private BreathingLevel _breathingLevel;
+
+
     private float _growSpeed = 2.0f;
     private float _minScale = 0.1f;
-    private float _maxScale = 4.0f;
+
 
     private void Start()
     {
-        _originalScale = transform.localScale;
-    }
+        SetSphereScale();
+    }    
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        InputHandler();
+    }
+
+    private void GetMaxScale()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, _originalScale * _maxScale, _growSpeed * Time.deltaTime);
+    }
+
+    private void GetMinScale()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, _originalScale * _minScale, _minScale * Time.deltaTime);
+    }   
+
+    private void SetSphereScale()
+    {
+        _originalScale = transform.localScale;
+    }  
+    
+    private void InputHandler()
+    {
+        if (_breathingLevel._isPauseOn == false)
         {
-            transform.localScale = Vector3.Lerp(transform.localScale, _originalScale * _maxScale, _growSpeed * Time.deltaTime);
-        }
-        else
-        {
-            transform.localScale = Vector3.Lerp(transform.localScale, _originalScale * _minScale, _minScale * Time.deltaTime);
-        }
+            if (Input.GetKeyDown(KeyCode.Space))
+                GetMaxScale();
+            else
+                GetMinScale();
+        }                
     }
 }
