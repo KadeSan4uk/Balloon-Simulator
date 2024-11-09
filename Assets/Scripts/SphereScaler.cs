1a20aa8da2e0f3854a5d1d018083c164cf20256d
KadeSan4uk <kotejchik@gmail.com>
2024-11-09 20:36:59 +0200

    Revert "fix"
    This reverts commit d0380b0d641616a2d9bcb04050f6ba08ee2e9a6f.

--- Assets/Scripts/SphereScaler.cs
+++ Assets/Scripts/SphereScaler.cs
@@ -1,51 +1,26 @@
-using System;
 using UnityEngine;
 
 public class SphereScaler : MonoBehaviour
 {
     private Vector3 _originalScale;
-
-    [SerializeField] private float _maxScale = 3.0f;
-    [SerializeField] private BreathingLevel _breathingLevel;
-
-
     private float _growSpeed = 2.0f;
     private float _minScale = 0.1f;
-
+    private float _maxScale = 4.0f;
 
     private void Start()
     {
-        SetSphereScale();
-    }    
+        _originalScale = transform.localScale;
+    }
 
     private void Update()
     {
-        InputHandler();
-    }
-
-    private void GetMaxScale()
-    {
-        transform.localScale = Vector3.Lerp(transform.localScale, _originalScale * _maxScale, _growSpeed * Time.deltaTime);
-    }
-
-    private void GetMinScale()
-    {
-        transform.localScale = Vector3.Lerp(transform.localScale, _originalScale * _minScale, _minScale * Time.deltaTime);
-    }   
-
-    private void SetSphereScale()
-    {
-        _originalScale = transform.localScale;
-    }  
-    
-    private void InputHandler()
-    {
-        if (_breathingLevel._isPauseOn == false)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-                GetMaxScale();
-            else
-                GetMinScale();
-        }                
+            transform.localScale = Vector3.Lerp(transform.localScale, _originalScale * _maxScale, _growSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, _originalScale * _minScale, _minScale * Time.deltaTime);
+        }
     }
 }
